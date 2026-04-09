a3797977fc69f98fc87f908b49bf7552ae16ede3
Sucuboy_core <107533003+kpro199procreator@users.noreply.github.com>
2026-04-09 16:12:12 -0600

    Delete logger.h

--- logger.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <whb/log.h>
-#include <whb/log_udp.h>
-#include <cstdio>
-
-#define log_printf(fmt, ...) WHBLogPrintf(fmt, ##__VA_ARGS__)
-
-static inline void log_init()   { WHBLogUdpInit(); }
-static inline void log_deinit() { WHBLogUdpDeinit(); }
